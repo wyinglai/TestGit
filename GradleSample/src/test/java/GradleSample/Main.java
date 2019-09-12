68bd74f28349b4f323a8c5e75d5c4c44a0f02934
eirai <eirai@DESKTOP-7RVOK2P.elecom>
2019-09-13 05:20:52 +0900

    最初のコミット

---
+++ GradleSample/src/test/java/GradleSample/Main.java
@@ -0,0 +1,21 @@
+package GradleSample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		Library lib = new Library();
+		if(lib.someLibraryMethod()) {
+			
+			System.out.println("OK");
+			
+		}
+		else {
+			
+			System.out.println("NG");
+		}
+
+	}
+
+}
